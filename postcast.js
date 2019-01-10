02a6589602ad47ae6fbf82a78dc9c577fb1f2e1e
Giappi <giappaig@gmail.com>
2019-01-10 17:28:59 +0000

    ~ Change program logic

--- postcast.js
+++ postcast.js
@@ -1,76 +1,97 @@
 
-/* global lesson_content, lesson_id */
 
-let spans = lesson_content.map((line, lineIndex) => line.Words.map
-(
-    (word) => 
-    {
-        var span = document.createElement("a");
-        let text = word.SW;
-        span.appendChild( document.createTextNode(text));
-        for(let attr in word)
-        {
-            span.setAttribute(attr, word[attr]);
-        }
-        let _audio      = "postcast-data/1-8969/" + word.WID + ".mp3";
-        let _pinyin     = word.PY;
-        
-        if(text == "A" || text == "B")
-        {
-            _audio      = "postcast-data/1-8969/" + lesson_id + "-2-" + (lineIndex + 1) + ".mp3";
-            _pinyin     = "--";
-        }
-        
-        let elementAttrs = 
-        {
-            href:       _pinyin != "" ? _audio : "",
-            title:      _pinyin
-        };
-        
-        for(let attr in elementAttrs)
-        {
-            if(elementAttrs[attr] != "")
-            {
-                span.setAttribute(attr, elementAttrs[attr]);
-            }
-        }
-        
-        // convert href to onclick
-        span.onclick = function(e)
-        {
-            new Audio(_audio + "").play();
-            return false;
-        };
-        if(span.href)
-        {
-            span.href = "#" + _audio;
-        }
-        
-        return span;
-    }
-));
+loadLesson(1, main);
 
-var postcastElement = document.getElementById("postcast");
-var ul              = document.createElement("ul");
-postcastElement.appendChild(ul);
-
-for(let sentence of spans)
+function main(data)
 {
-    var li = document.createElement("li");
-    ul.appendChild(li);
-    for(let word of sentence)
+    let lesson_content = Object.keys(data.Sents).map(e => data.Sents[e]).filter(e => !!(e.Words));
+    let lesson_id      = data.EID;
+    console.log(lesson_content);
+    let spans = lesson_content.map((line, lineIndex) => Object.keys(line.Words).map(e => line.Words[e]).map
+    (
+        (word) => 
+        {
+            var span = document.createElement("span");
+            let text = word.SW;
+            span.appendChild(document.createTextNode(text));
+            for(let attr in word)
+            {
+                span.setAttribute(attr, word[attr]);
+            }
+            let _audio      = "podcast/" + lesson_id + "/audio/" + word.WID + ".mp3";
+            let _pinyin     = word.PY;
+            let _meanning   = word.EN;
+            let _isWord      = _pinyin != "";
+            
+            if(text == "A" || text == "B")
+            {
+                _audio      = "podcast/" + lesson_id + "/audio/" + lesson_id + "-2-" + (lineIndex + 1) + ".mp3";
+                _pinyin     = "--";
+            }
+            
+            let elementAttrs = 
+            {
+                "class" :       _isWord ? "word" : "",
+                "href"  :       _isWord ? _audio : "",
+                "title" :       _pinyin + " / " + _meanning
+            };
+            
+            for(let attr in elementAttrs)
+            {
+                if(elementAttrs[attr] != "")
+                {
+                    span.setAttribute(attr, elementAttrs[attr]);
+                }
+            }
+            
+            // convert href to onclick
+            span.onclick = function(e)
+            {
+                new Audio(_audio + "").play();
+                return false;
+            };
+            if(span.href)
+            {
+                span.href = "#" + _audio;
+            }
+            
+            return span;
+        }
+    ));
+    
+    var postcastElement = document.getElementById("postcast");
+    var ul              = document.createElement("ul");
+    ul.setAttribute("class", "postcast");
+    postcastElement.appendChild(ul);
+    
+    for(let sentence of spans)
     {
-        li.appendChild(word);
+        var li = document.createElement("li");
+        ul.appendChild(li);
+        for(let word of sentence)
+        {
+            li.appendChild(word);
+            li.appendChild(document.createTextNode("  "));
+        }
     }
 }
 
 
+function loadData(url, callback)
+{
+    $.get(url, function(response)
+    {
+        callback(response);
+    });
+}
 
 
-
-
-
-
-
-
-
+function loadLesson(id, callback)
+{
+    loadData("podcast/" + id + "/lesson.json", function(data)
+    {
+        //console.log(typeof(data))
+        //callback(JSON.parse(data));
+        callback(data);
+    });
+}
