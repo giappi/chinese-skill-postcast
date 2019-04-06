e7e4a8576e89a25e641cfa801e3ec2f8e2086cfd
Giappi <giappaig@gmail.com>
2019-04-06 06:50:50 +0000

    - Improve Audio player. Add listing podcasts. Change audio format

--- postcast.js
+++ postcast.js
@@ -1,32 +1,33 @@
 
 
-loadLesson(11, main);
-
 function main(data)
 {
-    let lesson_content = Object.keys(data.Sents).map(e => data.Sents[e]).filter(e => !!(e.Words));
-    let lesson_id      = data.EID;
+    let player          = new Audio();
+    let lesson_content  = Object.keys(data.Sents).map(e => data.Sents[e]).filter(e => !!(e.Words));
+    let lesson_id       = data.EID;
     console.log(lesson_content);
     let spans = lesson_content.map((line, lineIndex) => Object.keys(line.Words).map(e => line.Words[e]).map
     (
         (word) => 
         {
             var span = document.createElement("span");
-            let text = word.SW;
+            let text = word.SW ? word.SW : "";
             span.appendChild(document.createTextNode(text));
             for(let attr in word)
             {
                 span.setAttribute(attr, word[attr]);
             }
-            let _audio      = "podcast/" + lesson_id + "/audio/" + word.WID + ".mp3";
+
             let _pinyin     = word.PY;
             let _meanning   = word.EN;
             let _isWord      = _pinyin != "";
+            let _audio      = "words/" + (_pinyin + "").replace("'", "-") + ".m4a";
             
             if(text == "A" || text == "B")
             {
-                _audio      = "podcast/" + lesson_id + "/audio/" + lesson_id + "-2-" + (lineIndex + 1) + ".mp3";
-                _pinyin     = "--";
+                _audio      = "podcast/" + lesson_id + "/audio/" + lesson_id + "-2-" + (lineIndex + 1) + ".m4a";
+                _pinyin     = "";
+                _meanning   = line.STRE;
             }
             
             let elementAttrs = 
@@ -47,7 +48,8 @@ function main(data)
             // convert href to onclick
             span.onclick = function(e)
             {
-                new Audio(_audio + "").play();
+                player.src=_audio + "";
+                player.play();
                 return false;
             };
             if(span.href)
@@ -90,8 +92,15 @@ function loadLesson(id, callback)
 {
     loadData("podcast/" + id + "/lesson.json", function(data)
     {
-        //console.log(typeof(data))
-        //callback(JSON.parse(data));
+        callback(data);
+    });
+}
+
+
+function loadPodcastList(callback)
+{
+    loadData("podcast-list.json", function(data)
+    {
         callback(data);
     });
 }
