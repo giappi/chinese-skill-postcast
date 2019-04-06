bf907371263c444e6e6520b54e9563ba90b2534d
Giappi <giappaig@gmail.com>
2019-04-06 12:33:30 +0000

    - Improve podcast lesson. Add font KaiTi

--- postcast.js
+++ postcast.js
@@ -3,64 +3,97 @@
 function main(data)
 {
     let player          = new Audio();
-    let lesson_content  = Object.keys(data.Sents).map(e => data.Sents[e]).filter(e => !!(e.Words));
+    console.log(Object.keys(data.Sents));
+    let lesson_content  = Object.keys(data.Sents).map(e => data.Sents[e]);
     let lesson_id       = data.EID;
     console.log(lesson_content);
-    let spans = lesson_content.map((line, lineIndex) => Object.keys(line.Words).map(e => line.Words[e]).map
-    (
-        (word) => 
+    let spans           = [];
+    let title_en        = data.TRE;
+    let title_cn        = data.ST;
+    for(let lineId in lesson_content)
+    {
+
+        if(lineId == "0")
         {
-            var span = document.createElement("span");
-            let text = word.SW ? word.SW : "";
-            span.appendChild(document.createTextNode(text));
-            for(let attr in word)
+            continue;
+        }
+
+        let line            = lesson_content[lineId];
+        let sentence_en     = line.STRE;
+        let sentenceHTML    = [];
+        var playSentButton  = document.createElement("span");
+        playSentButton.appendChild(document.createTextNode("[►]"));
+        playSentButton.classList = "clickable";
+        sentenceHTML.push(playSentButton);
+        playSentButton.onclick = function(e)
+        {
+            player.src = "podcast/" + lesson_id + "/audio/" + lesson_id + "-2-" + (lineId) + ".m4a";
+            player.play();
+            return false;
+        };
+        for(let wordId in line.Words)
+        {
+
+            if(wordId == "0")
             {
-                span.setAttribute(attr, word[attr]);
+                continue;
             }
 
+            let word        = line.Words[wordId];
             let _pinyin     = word.PY;
             let _meanning   = word.EN;
-            let _isWord      = _pinyin != "";
-            let _audio      = "words/" + (_pinyin + "").replace("'", "-") + ".m4a";
+            let text        = word.SW;
+
+            var span = document.createElement("span");
+            span.appendChild(document.createTextNode(text));
+
+            let _isWord     = _pinyin != "";
             
-            if(text == "A" || text == "B")
-            {
-                _audio      = "podcast/" + lesson_id + "/audio/" + lesson_id + "-2-" + (lineIndex + 1) + ".m4a";
-                _pinyin     = "";
-                _meanning   = line.STRE;
-            }
             
-            let elementAttrs = 
+            if(_isWord)
             {
-                "class" :       _isWord ? "word" : "",
-                "href"  :       _isWord ? _audio : "",
-                "title" :       _pinyin + " / " + _meanning
-            };
-            
-            for(let attr in elementAttrs)
-            {
-                if(elementAttrs[attr] != "")
+                for(let attr in word)
                 {
-                    span.setAttribute(attr, elementAttrs[attr]);
+                    span.setAttribute(attr, word[attr]);
+                }
+
+                let _audio          = "words/" + (_pinyin + "").replace("'", "-") + ".m4a";
+                let elementAttrs    = 
+                {
+                    "class" :       _isWord ? "word" : "",
+                    "href"  :       _isWord ? _audio : "",
+                    "title" :       _pinyin + " / " + _meanning
+                };
+
+                for(let attr in elementAttrs)
+                {
+                    if(elementAttrs[attr] != "")
+                    {
+                        span.setAttribute(attr, elementAttrs[attr]);
+                    }
+                }
+
+                // convert href to onclick
+                span.onclick = function(e)
+                {
+                    player.src=_audio + "";
+                    player.play();
+                    return false;
+                };
+                if(span.href)
+                {
+                    span.href = "#" + _audio;
                 }
             }
-            
-            // convert href to onclick
-            span.onclick = function(e)
-            {
-                player.src=_audio + "";
-                player.play();
-                return false;
-            };
-            if(span.href)
-            {
-                span.href = "#" + _audio;
-            }
-            
-            return span;
+            sentenceHTML.push(span);
         }
-    ));
+        
+        spans.push(sentenceHTML);
+    }
     
+    console.log(spans);
+    
+    var titleElement    = document.getElementById("title");
     var postcastElement = document.getElementById("postcast");
     var ul              = document.createElement("ul");
     ul.setAttribute("class", "postcast");
@@ -76,6 +109,10 @@ function main(data)
             li.appendChild(document.createTextNode("  "));
         }
     }
+
+    document.title = title_cn + " / " + title_en;
+    titleElement.innerText = document.title;
+
 }
 
 
